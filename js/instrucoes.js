aa304870e03bcc1bb997dbfd0be96f9d7a189e44
Wayne Rocha <dev.waynerocha@gmail.com>
2021-02-01 23:53:45 -0300

    Update instrucoes.js

--- js/instrucoes.js
+++ js/instrucoes.js
@@ -1,10 +1,10 @@
-// Slide show da demostração Brigado w3 school
-var myIndex = 0;
+
+let myIndex = 0;
 carousel();
 
 function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
+  let i;
+  let x = document.getElementsByClassName("mySlides");
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";
   }
